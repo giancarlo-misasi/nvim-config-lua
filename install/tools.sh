f9840b5c66ee77d34ad5d0c57efb5e57fc5faf2a
Giancarlo <98177309+giancarlo-misasi@users.noreply.github.com>
2023-03-09 08:46:44 -0800

    Fixed typo

--- install/tools.sh
+++ install/tools.sh
@@ -5,18 +5,18 @@ source $HOME/.zshrc
 
 # Install the SDKs
 echo "Checking for SDKs"
-install_latest rust asdf-community/asdf-rust
+asdf_install_latest rust asdf-community/asdf-rust
 cargo install ripgrep # for neovim searches
 cargo install fd-find # for neovim searches
 cargo install tree-sitter-cli # for neovim tree-sitter plugin
-install_latest python asdf-community/asdf-python
+asdf_install_latest python asdf-community/asdf-python
 pip install compiledb # for generating compilation database
 pip install boto3 # for aws sdk access
-install_latest ruby asdf-vm/asdf-ruby
-install_latest nodejs asdf-vm/asdf-nodejs
+asdf_install_latest ruby asdf-vm/asdf-ruby
+asdf_install_latest nodejs asdf-vm/asdf-nodejs
 npm install -g @devcontainers/cli # for devcontainer api
-install_latest lua Stratus3D/asdf-lua
-install_latest golang kennyp/asdf-golang
+asdf_install_latest lua Stratus3D/asdf-lua
+asdf_install_latest golang kennyp/asdf-golang
 
 # Install AWS Cli
 if ! [ -f /usr/local/bin/aws ]; then
